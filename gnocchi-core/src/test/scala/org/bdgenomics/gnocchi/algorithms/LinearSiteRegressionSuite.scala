b76dea46b4b28ee1c05743d17ac78fc2ae413a7e
Patrick Yang <patyang@berkeley.edu>
2017-10-12 00:10:45 -0700

    Add PIQ test

--- gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/algorithms/LinearSiteRegressionSuite.scala
+++ gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/algorithms/LinearSiteRegressionSuite.scala
@@ -24,42 +24,6 @@ import org.bdgenomics.gnocchi.primitives.phenotype.Phenotype
 import org.bdgenomics.gnocchi.primitives.variants.CalledVariant
 
 class LinearSiteRegressionSuite extends GnocchiFunSuite {
-
-  //    val phenotype = "MyPhenotype"
-  //    val altAllele = "No allele"
-  //    val locus = ReferenceRegion("Name", 1, 2)
-  //    val scOption = Option(sc)
-  //    val variant = new Variant()
-  //    val contig = new Contig()
-  //    //    contig.setContigName(locus.referenceName)
-  //    //  variant.setContig(contig)
-  //    variant.setStart(locus.start)
-  //    variant.setEnd(locus.end)
-  //    variant.setAlternateAllele(altAllele)
-  //    variant.setContigName("Name")
-  //    val phaseSetId = 0
-
-  //    test("rsquared should be 1 if all the points lie on a line") {
-  //
-  //      val observations = new Array[(Double, Array[Double])](10)
-  //      observations(0) = (0, Array[Double](1))
-  //      observations(1) = (0, Array[Double](1))
-  //      observations(2) = (0, Array[Double](1))
-  //      observations(3) = (0, Array[Double](1))
-  //      observations(4) = (1, Array[Double](2))
-  //      observations(5) = (1, Array[Double](2))
-  //      observations(6) = (1, Array[Double](2))
-  //      observations(7) = (2, Array[Double](3))
-  //      observations(8) = (2, Array[Double](3))
-  //      observations(9) = (2, Array[Double](3))
-  //
-  //      //use additiveLinearRegression to regress on Ascombe1
-  //      val regressionResult = AdditiveLinearRegression.applyToSite(observations, variant, phenotype, phaseSetId)
-  //
-  //      //Assert that the rsquared is in the right threshold.
-  //      assert(regressionResult.statistics("rSquared") == 1.0, "rSquared = " + regressionResult.statistics("rSquared"))
-  //    }
-
   // LinearSiteRegression.applyToSite correctness tests
   /* generate Anscombe's quartet for linear regression
     the quartet data is as follows:
@@ -240,109 +204,121 @@ class LinearSiteRegressionSuite extends GnocchiFunSuite {
   }
 
   ignore("LinearSiteRegression.applyToSite should work correctly for PIQ data.") {
-    //      /* Tests for multiple regression and covariate correction:
-    //      Data comes from here: https://onlinecourses.science.psu.edu/stat501/node/284
-    //      PIQ is treated as the phenotype of interest and Brain is treated as genotype. Height and Weight are treated as covariates.
-    //        Raw Data:
-    //                PIQ Brain Height  Weight
-    //                124 81.69 64.5  118
-    //                150 103.84  73.3  143
-    //                128 96.54 68.8  172
-    //                134 95.15 65.0  147
-    //                110 92.88 69.0  146
-    //                131 99.13 64.5  138
-    //                98  85.43 66.0  175
-    //                84  90.49 66.3  134
-    //                147 95.55 68.8  172
-    //                124 83.39 64.5  118
-    //                128 107.95  70.0  151
-    //                124 92.41 69.0  155
-    //                147 85.65 70.5  155
-    //                90  87.89 66.0  146
-    //                96  86.54 68.0  135
-    //                120 85.22 68.5  127
-    //                102 94.51 73.5  178
-    //                84  80.80 66.3  136
-    //                86  88.91 70.0  180
-    //                84  90.59 76.5  186
-    //                134 79.06 62.0  122
-    //                128 95.50 68.0  132
-    //                102 83.18 63.0  114
-    //                131 93.55 72.0  171
-    //                84  79.86 68.0  140
-    //                110 106.25  77.0  187
-    //                72  79.35 63.0  106
-    //                124 86.67 66.5  159
-    //                132 85.78 62.5  127
-    //                137 94.96 67.0  191
-    //                110 99.79 75.5  192
-    //                86  88.00 69.0  181
-    //                81  83.43 66.5  143
-    //                128 94.81 66.5  153
-    //                124 94.94 70.5  144
-    //                94  89.40 64.5  139
-    //                74  93.00 74.0  148
-    //                89  93.59 75.5  179
-    //    */
-    //      //load data into an observations variable
-    //      val observations = new Array[(Double, Array[Double])](38)
-    //      observations(0) = (81.69, Array[Double](124, 64.5, 118))
-    //      observations(1) = (103.84, Array[Double](150, 73.3, 143))
-    //      observations(2) = (96.54, Array[Double](128, 68.8, 172))
-    //      observations(3) = (95.15, Array[Double](134, 65.0, 147))
-    //      observations(4) = (92.88, Array[Double](110, 69.0, 146))
-    //      observations(5) = (99.13, Array[Double](131, 64.5, 138))
-    //      observations(6) = (85.43, Array[Double](98, 66.0, 175))
-    //      observations(7) = (90.49, Array[Double](84, 66.3, 134))
-    //      observations(8) = (95.55, Array[Double](147, 68.8, 172))
-    //      observations(9) = (83.39, Array[Double](124, 64.5, 118))
-    //      observations(10) = (107.95, Array[Double](128, 70.0, 151))
-    //      observations(11) = (92.41, Array[Double](124, 69.0, 155))
-    //      observations(12) = (85.65, Array[Double](147, 70.5, 155))
-    //      observations(13) = (87.89, Array[Double](90, 66.0, 146))
-    //      observations(14) = (86.54, Array[Double](96, 68.0, 135))
-    //      observations(15) = (85.22, Array[Double](120, 68.5, 127))
-    //      observations(16) = (94.51, Array[Double](102, 73.5, 178))
-    //      observations(17) = (80.80, Array[Double](84, 66.3, 136))
-    //      observations(18) = (88.91, Array[Double](86, 70.0, 180))
-    //      observations(19) = (90.59, Array[Double](84, 76.5, 186))
-    //      observations(20) = (79.06, Array[Double](134, 62.0, 122))
-    //      observations(21) = (95.50, Array[Double](128, 68.0, 132))
-    //      observations(22) = (83.18, Array[Double](102, 63.0, 114))
-    //      observations(23) = (93.55, Array[Double](131, 72.0, 171))
-    //      observations(24) = (79.86, Array[Double](84, 68.0, 140))
-    //      observations(25) = (106.25, Array[Double](110, 77.0, 187))
-    //      observations(26) = (79.35, Array[Double](72, 63.0, 106))
-    //      observations(27) = (86.67, Array[Double](124, 66.5, 159))
-    //      observations(28) = (85.78, Array[Double](132, 62.5, 127))
-    //      observations(29) = (94.96, Array[Double](137, 67.0, 191))
-    //      observations(30) = (99.79, Array[Double](110, 75.5, 192))
-    //      observations(31) = (88.00, Array[Double](86, 69.0, 181))
-    //      observations(32) = (83.43, Array[Double](81, 66.5, 143))
-    //      observations(33) = (94.81, Array[Double](128, 66.5, 153))
-    //      observations(34) = (94.94, Array[Double](124, 70.5, 144))
-    //      observations(35) = (89.40, Array[Double](94, 64.5, 139))
-    //      observations(36) = (93.00, Array[Double](74, 74.0, 148))
-    //      observations(37) = (93.59, Array[Double](89, 75.5, 179))
-    //
-    //      // use additiveLinearAssociation to regress on PIQ data
-    //      val regressionResult = AdditiveLinearRegression.applyToSite(observations, variant, phenotype, phaseSetId)
-    //
-    //      // check that the rsquared value is correct (correct is 0.2949)
-    //      assert(regressionResult.statistics("rSquared").asInstanceOf[Double] <= 0.2954)
-    //      assert(regressionResult.statistics("rSquared").asInstanceOf[Double] >= 0.2944)
-    //
-    //      // check that the p-value for Brain is correct (expectedPVal ~= 0.000855632)
-    //      val expectedLogPVal = -3.06771
-    //      assert(regressionResult.logPValue <= expectedLogPVal + 0.005)
-    //      assert(regressionResult.logPValue >= expectedLogPVal - 0.005)
+    /* Tests for multiple regression and covariate correction:
+    Data comes from here: https://onlinecourses.science.psu.edu/stat501/node/284
+    PIQ is treated as the phenotype of interest and Brain is treated as genotype. Height and Weight are treated as covariates.
+      Raw Data:
+              PIQ Brain Height  Weight
+              124 81.69 64.5  118
+              150 103.84  73.3  143
+              128 96.54 68.8  172
+              134 95.15 65.0  147
+              110 92.88 69.0  146
+              131 99.13 64.5  138
+              98  85.43 66.0  175
+              84  90.49 66.3  134
+              147 95.55 68.8  172
+              124 83.39 64.5  118
+              128 107.95  70.0  151
+              124 92.41 69.0  155
+              147 85.65 70.5  155
+              90  87.89 66.0  146
+              96  86.54 68.0  135
+              120 85.22 68.5  127
+              102 94.51 73.5  178
+              84  80.80 66.3  136
+              86  88.91 70.0  180
+              84  90.59 76.5  186
+              134 79.06 62.0  122
+              128 95.50 68.0  132
+              102 83.18 63.0  114
+              131 93.55 72.0  171
+              84  79.86 68.0  140
+              110 106.25  77.0  187
+              72  79.35 63.0  106
+              124 86.67 66.5  159
+              132 85.78 62.5  127
+              137 94.96 67.0  191
+              110 99.79 75.5  192
+              86  88.00 69.0  181
+              81  83.43 66.5  143
+              128 94.81 66.5  153
+              124 94.94 70.5  144
+              94  89.40 64.5  139
+              74  93.00 74.0  148
+              89  93.59 75.5  179
+  */
+    //load data into an observations variable
+    val observations = new Array[(Double, Array[Double])](38)
+    observations(0) = (81.69, Array[Double](124, 64.5, 118))
+    observations(1) = (103.84, Array[Double](150, 73.3, 143))
+    observations(2) = (96.54, Array[Double](128, 68.8, 172))
+    observations(3) = (95.15, Array[Double](134, 65.0, 147))
+    observations(4) = (92.88, Array[Double](110, 69.0, 146))
+    observations(5) = (99.13, Array[Double](131, 64.5, 138))
+    observations(6) = (85.43, Array[Double](98, 66.0, 175))
+    observations(7) = (90.49, Array[Double](84, 66.3, 134))
+    observations(8) = (95.55, Array[Double](147, 68.8, 172))
+    observations(9) = (83.39, Array[Double](124, 64.5, 118))
+    observations(10) = (107.95, Array[Double](128, 70.0, 151))
+    observations(11) = (92.41, Array[Double](124, 69.0, 155))
+    observations(12) = (85.65, Array[Double](147, 70.5, 155))
+    observations(13) = (87.89, Array[Double](90, 66.0, 146))
+    observations(14) = (86.54, Array[Double](96, 68.0, 135))
+    observations(15) = (85.22, Array[Double](120, 68.5, 127))
+    observations(16) = (94.51, Array[Double](102, 73.5, 178))
+    observations(17) = (80.80, Array[Double](84, 66.3, 136))
+    observations(18) = (88.91, Array[Double](86, 70.0, 180))
+    observations(19) = (90.59, Array[Double](84, 76.5, 186))
+    observations(20) = (79.06, Array[Double](134, 62.0, 122))
+    observations(21) = (95.50, Array[Double](128, 68.0, 132))
+    observations(22) = (83.18, Array[Double](102, 63.0, 114))
+    observations(23) = (93.55, Array[Double](131, 72.0, 171))
+    observations(24) = (79.86, Array[Double](84, 68.0, 140))
+    observations(25) = (106.25, Array[Double](110, 77.0, 187))
+    observations(26) = (79.35, Array[Double](72, 63.0, 106))
+    observations(27) = (86.67, Array[Double](124, 66.5, 159))
+    observations(28) = (85.78, Array[Double](132, 62.5, 127))
+    observations(29) = (94.96, Array[Double](137, 67.0, 191))
+    observations(30) = (99.79, Array[Double](110, 75.5, 192))
+    observations(31) = (88.00, Array[Double](86, 69.0, 181))
+    observations(32) = (83.43, Array[Double](81, 66.5, 143))
+    observations(33) = (94.81, Array[Double](128, 66.5, 153))
+    observations(34) = (94.94, Array[Double](124, 70.5, 144))
+    observations(35) = (89.40, Array[Double](94, 64.5, 139))
+    observations(36) = (93.00, Array[Double](74, 74.0, 148))
+    observations(37) = (93.59, Array[Double](89, 75.5, 179))
+
+    // use additiveLinearAssociation to regress on PIQ data
+    val (genotypes, phenotypes) = observations.unzip
+    val genotypeStates = genotypes.toList.zipWithIndex.map(item => GenotypeState(item._2.toString, item._1.toString))
+    val cv = CalledVariant(1, 1, "rs123456", "A", "C", "", "", "", "", genotypeStates)
+
+    val phenoMap = phenotypes
+      .toList
+      .zipWithIndex
+      .map(item => (item._2.toString, Phenotype(item._2.toString, "pheno1", item._1(0), item._1.slice(1, 3).toList)))
+      .toMap
+
+    // use additiveLinearRegression to regress on PIQ
+    val regressionResult = AdditiveLinearRegression.applyToSite(phenoMap, cv)
+
+    // Assert that the rsquared is in the right threshold.
+    // R^2 = 1 - (SS_res / SS_tot)
+    val rSquared = 1 - regressionResult.ssResiduals / regressionResult.ssDeviations
+    val expectedRSquared = 0.2954
+    assert(nearby(rSquared, expectedRSquared, 0.005))
+
+    // Assert that the p-value for Brain is correct (expectedPVal ~= 0.000855632)
+    val expectedPVal = 0.000855632
+    assert(nearby(regressionResult.pValue, expectedPVal, 0.00005))
   }
+
+  // LinearSiteRegression.applyToSite input validation tests
 
   ignore("LinearSiteRegression.applyToSite should break gracefully on a singular matrix") {
 
   }
-
-  // LinearSiteRegression.applyToSite input validation tests
 
   ignore("LinearSiteRegression.applyToSite should break when there is not overlap between sampleIDs in phenotypes and CalledVariant objects.") {
 
