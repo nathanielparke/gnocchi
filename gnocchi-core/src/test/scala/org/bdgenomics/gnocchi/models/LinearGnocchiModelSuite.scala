f84d3937c19b64d82d6ecd19dccfdf56532a8215
Adithya Girish <adithyagirish@berkeley.edu>
2017-10-16 11:52:18 -0700

    adding more stubs to new tests

---
+++ gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/models/LinearGnocchiModelSuite.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.gnocchi.models
+
+/**
+  * Created by adithya on 10/16/17.
+  */
+class LinearGnocchiModelSuite {
+
+}
---
+++ gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/models/variant/LogisticVariantModelSuite.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.gnocchi.models.variant
+
+/**
+  * Created by adithya on 10/16/17.
+  */
+class LogisticVariantModelSuite {
+
+}
