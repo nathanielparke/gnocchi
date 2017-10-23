6363958ba6ffb55645170ec69a5254a265ee16df
Kunal Gosar <gosarkunal9@gmail.com>
2017-10-23 00:22:10 -0700

    Create testing stubs for variant models

---
+++ gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/models/variant/LinearVariantModelSuite.scala
@@ -0,0 +1,16 @@
+package org.bdgenomics.gnocchi.models.variant
+
+import org.bdgenomics.gnocchi.GnocchiFunSuite
+
+class LinearVariantModelSuite extends GnocchiFunSuite {
+ ignore("Functional test of LinearVariantModel.mergeWith")
+
+ ignore("Functional test of LinearVariantModel.updateSsDeviations")
+
+ ignore("Functional test of LinearVariantModel.updateSsResiduals")
+
+ ignore("Functional test of LinearVariantModel.computeGeneticParameterStandardError")
+
+ ignore("Functional test of LinearVariantModel.updateResidualDegreesOfFreedom")
+
+}
---
+++ gnocchi-core/src/test/scala/org/bdgenomics/gnocchi/models/variant/LogisticVariantModelSuite.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.gnocchi.models.variant
+
+import org.bdgenomics.gnocchi.GnocchiFunSuite
+
+class LogisticVariantModelSuite extends GnocchiFunSuite {
+  ignore("Functional test of LogisticVariantModel.mergeWith")
+
+}
