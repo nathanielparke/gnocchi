aca9e7eb650f3d74776a87462d0d28c9a3a0ea9c
Adithya Girish <adithyagirish@berkeley.edu>
2017-10-29 22:40:08 -0700

    adding more of the wrapper

--- gnocchi-apis/src/main/scala/org/bdgenomics/gnocchi/api/java/JavaLinearGnocchiModel.scala
+++ gnocchi-apis/src/main/scala/org/bdgenomics/gnocchi/api/java/JavaLinearGnocchiModel.scala
@@ -3,11 +3,27 @@ package org.bdgenomics.gnocchi.api.java
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.Dataset
 import org.bdgenomics.gnocchi.models.variant.{ QualityControlVariantModel, LinearVariantModel }
-import org.bdgenomics.gnocchi.models.{ GnocchiModelMetaData, GnocchiModel, LinearGnocchiModel }
+import org.bdgenomics.gnocchi.models.{ LinearGnocchiModelFactory, GnocchiModel, LinearGnocchiModel }
 import org.bdgenomics.gnocchi.primitives.phenotype.Phenotype
 import org.bdgenomics.gnocchi.primitives.variants.CalledVariant
 
-// (TODO) Add factory apply
+object JavaLinearGnocchiModelFactory {
+  def apply(genotypes: Dataset[CalledVariant],
+            phenotypes: Broadcast[Map[String, Phenotype]],
+            phenotypeNames: Option[List[String]],
+            QCVariantIDs: Option[Set[String]] = None,
+            QCVariantSamplingRate: java.lang.Double = 0.1,
+            allelicAssumption: java.lang.String = "ADDITIVE",
+            validationStringency: java.lang.String = "STRICT"): LinearGnocchiModel = {
+    LinearGnocchiModelFactory(genotypes,
+      phenotypes,
+      phenotypeNames,
+      QCVariantIDs,
+      QCVariantSamplingRate,
+      allelicAssumption,
+      validationStringency)
+  }
+}
 
 class JavaLinearGnocchiModel(val lgm: LinearGnocchiModel) {
   def mergeGnocchiModel(otherModel: GnocchiModel[LinearVariantModel, LinearGnocchiModel]): GnocchiModel[LinearVariantModel, LinearGnocchiModel] = {
---
+++ gnocchi-python/bdgenomics/gnocchi/linearGnocchiModel.py
@@ -0,0 +1,48 @@
+# Licensed to Big Data Genomics (BDG) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The BDG licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from bdgenomics.gnocchi.primitives import CalledVariantDataset, PhenotypeMap, LinearVariantModelDataset
+
+class LinearGnocchiModel(object):
+
+    def __init__(self, ss,
+                 genotypes,
+                 phenotypes,
+                 phenotypeNames,
+                 QCVariantIDs,
+                 QCVariantSamplingRate,
+                 allelicAssumption,
+                 validationStringency):
+        self._sc = ss.sparkContext
+        self._jvm = self._sc._jvm
+        self.__jlgm = self._jvm.org.bdgenomics.gnocchi.api.java.JavaLinearGnocchiModel(genotypes,
+                                                                                       phenotypes,
+                                                                                       phenotypeNames,
+                                                                                       QCVariantIDs,
+                                                                                       QCVariantSamplingRate,
+                                                                                       allelicAssumption,
+                                                                                       validationStringency)
+    def mergeGnocchiModel(self, otherModel):
+        return self.__jlgm.mergeGnocchiModel(otherModel)
+
+    def mergeVariantModels(self, newVariantModels):
+        dataset = self.__jlgm.mergeVariantModels(newVariantModels)
+        return LinearVariantModelDataset(dataset, self._sc)
+
+    def mergeQCVariants(self, newQCVariantModels):
+        dataset = self.__jlgm.mergeQCVariants(newQCVariantModels)
+        return CalledVariantDataset(dataset, self._sc)
+
