d890a478531c4d8eca82e8d8aa3f570cafa24cae
Kay Ousterhout <kay@kelda.io>
2017-11-13 10:50:50 -0800

    blueprint: Define machines, use latest API
    This commit changes the blueprint to define the machines it uses
    (rather than using base infrastructure) and to use the latest
    version of the Kelda API (so to call deploy() on Spark rather
    than on the Kelda Infrastructure object).

---
+++ kelda/deploy.js
@@ -0,0 +1,18 @@
+const kelda = require('kelda');
+const spark = require('@kelda/spark');
+
+const numSparkWorkers = 16;
+
+// Initialize the virtual machines to run Spark.
+const machine = new kelda.Machine({ provider: 'Amazon', size: 'm3.xlarge' });
+
+// Create one machine for each Spark worker, plus one extra machine to run
+// the Spark master and job driver.
+const keldaWorkers = machine.replicate(numSparkWorkers + 1);
+const infra = new kelda.Infrastructure(machine, keldaWorkers);
+
+spark.setImage("kayousterhout/gnocchi")
+const s = new spark.Spark(numSparkWorkers,
+  { memoryMB: spark.getWorkerMemoryMB(machine) });
+s.exposeUIToPublic();
+s.deploy(infra);
