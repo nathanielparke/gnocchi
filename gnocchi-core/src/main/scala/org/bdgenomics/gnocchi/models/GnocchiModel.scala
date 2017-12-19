eafe514f2ceb3741fc0dd9c049e5641df6748283
Nathaniel Parke <nathaniel.parke@gmail.com>
2017-12-19 09:47:34 -0800

    make absolute value

--- gnocchi-core/src/main/scala/org/bdgenomics/gnocchi/models/GnocchiModel.scala
+++ gnocchi-core/src/main/scala/org/bdgenomics/gnocchi/models/GnocchiModel.scala
@@ -227,14 +227,14 @@ trait GnocchiModel[VM <: VariantModel[VM], GM <: GnocchiModel[VM, GM]] {
     val covarVals = variantModels.repartition(40).rdd.flatMap(f => {
       val weights = DenseVector(f.association.weights.drop(2): _*)
       covarMat.map(g => {
-        ((f.uniqueID, g._1), (f.association.weights(1), g._2 * weights + f.association.weights(0), f.association.geneticParameterScore))
+        ((f.uniqueID, g._1), (f.association.weights(1), g._2 * weights + f.association.weights(0), math.abs(f.association.geneticParameterScore)))
       })
-    }).filter(f => !f._2._2.isNaN)
+    }).filter(f => !f._2._2.isNaN && !f._2._3.isNaN)
 
     val genotypes_2 = genotypes.repartition(40).rdd.flatMap(f => f.samples.map(g => ((f.uniqueID, g.sampleID), g.alts.toDouble)))
 
     val y_hat = genotypes_2.join(covarVals).map(f => {
-      // f = ((rsID, sampleID), (alts (genoBeta, beta*covar)))
+      // f = ((rsID, sampleID), (alts (genoBeta, beta*covar, geneticParameterScore)))
       (f._1._2, (f._2._1 * f._2._2._1 + f._2._2._2, f._2._2._3))
     })
 
