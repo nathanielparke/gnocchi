3d3369396d56c1b6881dada458f8dacb3d49859b
Nathaniel Parke <nathaniel.parke@gmail.com>
2018-01-30 08:51:00 -0800

    get kelda working

--- kelda/deploy.js
+++ kelda/deploy.js
@@ -1,17 +1,18 @@
 const kelda = require('kelda');
 const spark = require('@kelda/spark');
 
-const numSparkWorkers = 16;
+const numSparkWorkers = 5;
 
 // Initialize the virtual machines to run Spark.
-const machine = new kelda.Machine({ provider: 'Amazon', size: 'm3.xlarge' });
+const machine = new kelda.Machine({ provider: 'Amazon', size: 'm4.large' });
 
 // Create one machine for each Spark worker, plus one extra machine to run
 // the Spark master and job driver.
 const keldaWorkers = machine.replicate(numSparkWorkers + 1);
 const infra = new kelda.Infrastructure(machine, keldaWorkers);
 
-spark.setImage("kayousterhout/gnocchi")
+spark.setImage(new kelda.Image('gnocchi',
+  fs.readFileSync('./Dockerfile', { encoding: 'utf8' })));
 const s = new spark.Spark(numSparkWorkers,
   { memoryMiB: spark.getWorkerMemoryMiB(machine) });
 s.exposeUIToPublic();
