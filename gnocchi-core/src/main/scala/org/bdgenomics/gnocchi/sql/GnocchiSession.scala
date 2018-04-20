272725a676908eb039bc71b49c4f3c725cfacb46
Nathaniel Parke <nathaniel.parke@gmail.com>
2018-04-20 09:59:22 -0700

    cache

--- gnocchi-core/src/main/scala/org/bdgenomics/gnocchi/sql/GnocchiSession.scala
+++ gnocchi-core/src/main/scala/org/bdgenomics/gnocchi/sql/GnocchiSession.scala
@@ -98,6 +98,8 @@ class GnocchiSession(@transient val sc: SparkContext)
     require(mind >= 0.0 && mind <= 1.0,
       "`mind` value must be between 0.0 to 1.0 inclusive.")
 
+    genotypes.cache()
+
     val x = genotypes.rdd.flatMap(
       f => {
         f.samples.map(
