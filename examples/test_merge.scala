1b0f56adc9b04dc73410150e6ef5e4c5bd87f86d
Nathaniel Parke <nathaniel.parke@gmail.com>
2018-09-06 14:42:41 -0700

    fixes example

--- examples/test_merge.scala
+++ examples/test_merge.scala
@@ -4,11 +4,11 @@ import org.bdgenomics.gnocchi.sql.LinearAssociationsDatasetBuilder
 
 
 val genos1 = sc.loadGenotypes("./testData/merge/time_genos_1.vcf", "time_genos_1", "ADDITIVE")
-val phenos1 = sc.loadPhenotypes("./testData/merge/time_phenos_1.txt", "IID", "pheno_1", "\t", Option("examples/testData/merge/time_phenos_1.txt"), Option(List("pheno_2", "pheno_3")), "\t")
+val phenos1 = sc.loadPhenotypes("./testData/merge/time_phenos_1.txt", "IID", "pheno_1", "\t", Option("./testData/merge/time_phenos_1.txt"), Option(List("pheno_2", "pheno_3")), "\t")
 val linearResults1 = LinearSiteRegression(genos1, phenos1)
 
 val genos2 = sc.loadGenotypes("./testData/merge/time_genos_2.vcf", "time_genos_2", "ADDITIVE")
-val phenos2 = sc.loadPhenotypes("./testData/merge/time_phenos_2.txt", "IID", "pheno_1", "\t", Option("examples/testData/merge/time_phenos_2.txt"), Option(List("pheno_2", "pheno_3")), "\t")
+val phenos2 = sc.loadPhenotypes("./testData/merge/time_phenos_2.txt", "IID", "pheno_1", "\t", Option("./testData/merge/time_phenos_2.txt"), Option(List("pheno_2", "pheno_3")), "\t")
 val linearResults2 = LinearSiteRegression(genos2, phenos2)
 
 val model1 = linearResults1.gnocchiModel
@@ -20,12 +20,8 @@ val builder = LinearAssociationsDatasetBuilder(merged, genos1, phenos1)
 
 val updatedBuilder = builder.addNewData(genos2, phenos2)
 
-val outPath = "../mergedAssociations/"
-
-updatedBuilder.saveAssociations(outPath)
-
 val mergedGenos = sc.loadGenotypes("./testData/merge/merged.vcf", "merged_vcf", "ADDITIVE")
-val mergedPhenos = sc.loadPhenotypes("./testData/merge/merged_phenos.txt", "IID", "pheno_1", "\t", Option("examples/testData/merge/merged_phenos.txt"), Option(List("pheno_2", "pheno_3")), "\t")
+val mergedPhenos = sc.loadPhenotypes("./testData/merge/merged_phenos.txt", "IID", "pheno_1", "\t", Option("./testData/merge/merged_phenos.txt"), Option(List("pheno_2", "pheno_3")), "\t")
 val mergedAssociations = LinearSiteRegression(mergedGenos, mergedPhenos)
 
 val sortedManualMergeAssociations = updatedBuilder.linearAssociationBuilders.map(_.association).sort($"pValue".asc)
